edb2f6edb1b77c2d7b205c938756735473999133
Kumar <kumarbalakrishnan.j@supashock.com>
2021-10-26 08:35:06 +1030

    Commit from Work system

--- test.py
+++ test.py
@@ -1,7 +1,110 @@
-name = "Jhon Smith"
-age = 20
+# name = "Jhon Smith"
+# age = 20
+# new_patient = True
+#
+# if new_patient:
+#     print("Patient name =", name)
+#     print("Patient Age = ", age)
+#
+
+# obtaining Input
+
+name = input("Enter your name ")
+birth_year = input (" Enter birth year")
+birth_year= int(birth_year)
+age = 2021 - birth_year
 new_patient = True
 
 if new_patient:
     print("Patient name =", name)
     print("Patient Age = ", age)
+
+
+# String
+
+# Name = "Saravana Kumar"
+# print(Name.find("Kumar"))
+# print(Name.casefold())
+# print(Name.replace("Kumar", "Janaki"))
+# print("Kumar" in Name)
+
+
+# Arithmetic Operations
+
+# X = (10 +5) * 5
+# print(X)
+
+
+# Comparison
+
+# X = 3 > 2
+#
+# if X:
+#     print("Greater")
+
+
+
+# Logical Operators
+
+# age = 25
+#
+# print(age >10 and age <30)
+#
+# print(age>10 or age <30)
+#
+# print( not age > 30)
+
+
+# If statements
+
+# Temp = 30
+#
+# if Temp >20:
+#     print("its hot")
+# elif Temp < 15:
+#     print("its cool")
+
+#while loops
+
+# i = 1
+# while i <= 5:
+#     print(i * '*')
+#     i = i+1s
+
+# Lists
+
+# names = ["saravana", "Kumar", "Balakrishnan", "Janaki"]
+# print(names[0])
+# print(names[1:4])
+# for name in names:
+#     print(name)
+
+# List functions
+#
+# names = ["saravana", "Kumar", "Balakrishnan", "Janaki"]
+# names.append("jhon")
+# print(names)
+# names.remove("jhon")
+# print(names)
+# print("Kumar" in names)
+# print(len(names))
+
+
+# for loops
+# names = ["saravana", "Kumar", "Balakrishnan", "Janaki"]
+#
+# for name in names:
+#     print(name)
+#
+# for name in range(4):
+#     print(name)
+#
+# number = range(4,10)
+# for i in number:
+#     print(i)
+
+
+# Tuples , immutable
+
+# numbers = (1,2,3)
+# print(numbers[0])
